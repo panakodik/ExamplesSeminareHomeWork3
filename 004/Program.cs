da662f3354c1e5342b441b05c5875171c95d07db
Aleksandr <pandom@yandex.ru>
2023-01-27 00:45:53 +0300

    004-Кубы чисел четные

---
+++ 004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+Console.WriteLine("Показать кубы чисел, заканчивающихся на четную цифру в интервале от 1 до N");
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++)
+{
+    int a = i * i * i;
+    if(a % 2 == 0)
+    {
+        Console.WriteLine("Куб числа " + i + " равен " + a + " - заканчивается на четкую цифру ");
+    }
+}
