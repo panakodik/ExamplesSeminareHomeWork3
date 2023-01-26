613d10243f4c21c7330faa78dfbca35c618414d2
Aleksandr <pandom@yandex.ru>
2023-01-27 00:43:25 +0300

    001-Кубы числе от 1 до N

---
+++ 001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Найти кубы чисел от 1 до N");
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine("Квадрат числа " + i + " равен: " + i * i * i);
+}
+Console.WriteLine("Получили возведение в квадрат чисел от 1 до " + n);
