a2535fffa2313dcb876954f158e39e61d04d9f56
Aleksandr <pandom@yandex.ru>
2023-01-27 00:41:58 +0300

    003-Произведение чисел от 1 до N

---
+++ 003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Clear();
+Console.WriteLine("Программа вычисления произведения чисел от 1 до N");
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+int pow = 1;
+for (int i = 1; i <= n; i++)
+{
+    pow = pow * i;
+}
+Console.WriteLine("Получили произведения чисел от 1 до " + n + " равно " + pow);
