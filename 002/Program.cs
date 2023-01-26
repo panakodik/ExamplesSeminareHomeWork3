fa5fa280331e8e1aa524dd4fe96825201cecaa2e
Aleksandr <pandom@yandex.ru>
2023-01-27 00:44:32 +0300

    002-Сумма цифр в числе

---
+++ 002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+Console.WriteLine("Подсчитать сумму цифр в числе");
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+int sum = 0;
+
+while (N > 0)
+{
+    int n = N % 10;
+    sum = sum + n;
+    N = N / 10;
+}
+
+Console.WriteLine("Сумма цифр в числе = " + sum);
